b13d620dcff8230cea68bbecdabb5c42cf8467bd
Vitaly Polyakov <vit.polyakov71@gmail.com>
2025-07-20 15:28:33 +0300

    Add widget.py template

---
+++ src/widget.py
@@ -0,0 +1,9 @@
+def mask_account_card_widget(full_data: str) -> str:
+    """Маскирует номер карты/счета (формат: 'Visa Platinum 7000792289606361')"""
+    # Ваш код здесь
+    pass
+
+def get_date(datetime_str: str) -> str:
+    """Конвертирует '2024-03-11T02:26:18.671407' → '11.03.2024'"""
+    # Ваш код здесь
+    pass
